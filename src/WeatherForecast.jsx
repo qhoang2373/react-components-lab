3692b80cbf1db85dbc7652bf06cab4a044bf1b48
Quan <qhoang2373@gmail.com>
2024-11-10 20:19:45 -0800

    complete assignment

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -3,10 +3,10 @@ import "./WeatherForecast.css"
 const WeatherForecastBit = ({weatherForecast}) => {
     return (
         <div className="weather">
-                <h2>{weatherForecast.day}</h2>
-                <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />
-                <p><span>Conditions: </span>{weatherForecast.conditions}</p>
-                <p><span>Time: </span>{weatherForecast.time}</p>
+                <h2>{weatherForecast.day}</h2> 
+                <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />  {/* sets the image sources*/}
+                <p><span>Conditions: </span>{weatherForecast.conditions}</p> {/* displays the weather condition*/}
+                <p><span>Time: </span>{weatherForecast.time}</p>  {/* displays the time condition*/}
         </div>
     )
 }
